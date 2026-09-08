ec30c70e81e7af66bff042f144e82fbf8ea14bd1
jim850502 <jim850502@gmail.com>
2026-09-08 15:07:51 +0800

    Activate new service worker immediately and clear old caches

--- sw.js
+++ sw.js
@@ -1,4 +1,19 @@
-const CACHE='taxi-fare-v35';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))) });
+const CACHE='taxi-fare-v36';
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./manifest.webmanifest'])))});
+self.addEventListener('activate',event=>event.waitUntil(Promise.all([
+  caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))),
+  self.clients.claim()
+])));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(fetch(event.request).then(response=>{
+    if(response.ok&&new URL(event.request.url).origin===self.location.origin){
+      const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    }
+    return response;
+  }).catch(()=>caches.match(event.request)));
+});
