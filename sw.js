1b6945c5dc0fde2156603d2cc7ece436ab61c4c6
jim850502 <jim850502@gmail.com>
2026-09-08 13:46:13 +0800

    Refresh offline cache for OCR value corrections

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='taxi-fare-v31';
+const CACHE='taxi-fare-v32';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))) });
